d6fe10a49e0d188065c212022369fb774f9d98fc
Salif Moctar Konate <82183797+salif18@users.noreply.github.com>
2024-08-24 22:30:14 +0000

    Update store

--- app/src/app/context/store.js
+++ app/src/app/context/store.js
@@ -46,29 +46,7 @@ export const MyStoreProvider = (props) => {
     setPanier([...panier, { ...item, qty: 1 }]);
   };
  
-  //enregistrer ou effectuer une vente
-  const handleVendre = async () => {
-  try {
-    // Créer une liste de promesses pour chaque requête axios
-    const promises = panier.map((item) => {
-      return axios.post(`/api/ventes`, datePersonaliser ? {...item, timestamps: datePersonaliser} : item);
-    });
-
-    // Attendre que toutes les requêtes soient terminées
-    const responses = await Promise.all(promises);
-
-    // Récupérer les messages de succès de chaque réponse
-    const messages = responses.map((response) => response.data.message);
-    
-    // Afficher tous les messages de succès
-    setMessage(messages.join('\n'));
-
-  } catch (error) {
-    // Gestion des erreurs
-    console.error('Erreur lors de la gestion des ventes:', error);
-    setMessage('Une erreur s\'est produite lors de l\'enregistrement des ventes.');
-  }
-};
+ 
 
   //envoyer les depenses
   const sendDepensesToDataBase = (item)=> {
