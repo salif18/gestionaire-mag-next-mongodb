6132f9d71df86747b36725780b2011409901cc54
salif18 <salifmoctarkonate@gmail.com>
2024-09-03 00:39:12 +0000

    modification de fichier store

--- app/src/app/components/Revenues.js
+++ app/src/app/components/Revenues.js
@@ -7,10 +7,10 @@ const Revenues = ({depensesTotal ,benefice}) => {
     
     const result = benefice - depensesTotal
     return (
-        <div className='revenusWidget'>
+        <section className='revenusWidget'>
         <h1><TrendingDownIcon style={{color:result <= 0 ? 'red':''}} className='icon'/> Etat de caisse</h1>
-        <div className='r'><p>{result} Fcfa</p> {result <= 0 ?<ArrowDownwardIcon style={{color:'red'}}/> :<ArrowUpwardIcon style={{color:'green'}} />}</div>
-    </div>
+        <section className='r'><p>{result} Fcfa</p> {result <= 0 ?<ArrowDownwardIcon style={{color:'red'}}/> :<ArrowUpwardIcon style={{color:'green'}} />}</section>
+    </section>
     );
 }
 
