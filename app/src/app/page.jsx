1c9be5b31199720d461305a32f5e9cc419f47681
salif18 <salifmoctarkonate@gmail.com>
2024-09-12 23:04:06 +0000

    d

--- app/src/app/page.jsx
+++ app/src/app/page.jsx
@@ -33,7 +33,11 @@ const Login = () => {
         e.preventDefault();
         if (user.contacts.length > 0 && user.password.length > 0) {
             try {
-                const res = await axios.post(`${process.env.NEXT_PUBLIC_URI}/auth/login`, user);
+                const res = await axios.post(`${process.env.NEXT_PUBLIC_URI}/auth/login`,{
+                    headers: {
+                      'Content-Type': 'application/json',
+                      'Authorization': `Bearer `,
+                    }}, user);
                 const data = await res.data;
 
                 if (res.status === 200) { // Vérification du statut de la réponse
