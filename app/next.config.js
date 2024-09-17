05ad49738ba3c0d37f10f4c9c8d6ad9deae9a8ba
salif18 <salifmoctarkonate@gmail.com>
2024-09-17 22:35:49 +0000

    k

--- app/next.config.js
+++ app/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['https://salespulse-api.vercel.app/api'], // Remplacez par le domaine de votre serveur
-      },
+   
 
 }
 
