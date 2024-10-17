99a8784d795dd7fd475d579e772f7771fba59e6d
salif18 <salifmoctarkonate@gmail.com>
2024-10-17 21:43:53 +0000

    eeee

--- app/app/components/StatsWeek.js
+++ app/app/components/StatsWeek.js
@@ -20,7 +20,7 @@ const StatsWeek = ({data}) => {
   return (
     <article className='statsWeekwidget'>
     <h1>Statistiques Hebdomadaire</h1>
-    <section style={{ flex:1, height: 300}}>
+    <section style={{ width: '100%', height: 300}}>
     <ResponsiveContainer width="100%" height="100%">
     <BarChart width={750} height={300} data={chartData} barSize={25} margin={{ top: 20, right: 30, bottom: 5 }}>
       <CartesianGrid strokeDasharray="3 3" />
