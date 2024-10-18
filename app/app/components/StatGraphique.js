eaf98b02243cc33718ba4fb944b464e05e910cd6
salif18 <salifmoctarkonate@gmail.com>
2024-10-18 10:51:27 +0000

    hauteru de statts modifier

--- app/app/components/StatGraphique.js
+++ app/app/components/StatGraphique.js
@@ -15,7 +15,7 @@ const StatGraphique = ({ data }) => {
   return (
     <div className='stats-graph-widget'>
       <h1>Interprétation graphique</h1>
-      <div style={{ height: '300px'}}>
+      <div style={{ height: '250px'}}>
         <ResponsiveContainer>
           <AreaChart data={chartData} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
             <defs>
