2fa731856baa46975cd75485d5bf5e182fc83462
salif18 <salifmoctarkonate@gmail.com>
2024-10-20 18:59:23 +0000

    changement de couleur

--- app/app/components/layouts/Navbar.js
+++ app/app/components/layouts/Navbar.js
@@ -12,7 +12,6 @@ const Navbar = () => {
  
   return (
     <header className='header'>
-     <button className='menu-berger' ><MenuIcon /></button>
       <section className="header-left">
         <Image src={image} alt='' />
         <section className='column'>
